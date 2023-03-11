10b2f696b3bf107c0b9461cd376b48a405a6744f
long <75117606+Dohoanglong15041@users.noreply.github.com>
2023-03-11 11:45:15 +0700

    Update News.php

--- app/News.php
+++ app/News.php
@@ -22,4 +22,22 @@ class News extends Model
 	{
 		return $this->belongsTo('App\Category','cat_id');
 	}
+	class News extends Model
+{
+    protected $table ='news';
+	protected $guarded =[];
+
+	public function category()
+	{
+		return $this->belongsTo('App\Category','cat_id');
+	}
+}
+{
+    protected $table ='news';
+	protected $guarded =[];
+
+	public function category()
+	{
+		return $this->belongsTo('App\Category','cat_id');
+	}
 }
